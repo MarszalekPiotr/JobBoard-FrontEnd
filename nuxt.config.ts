402eb8799a90f9479d2150e788da2562fe84ae8b
MarszalekPiotr <marszalekpiotr183@gmail.com>
2024-04-30 22:32:14 +0200

    Configuration

---
+++ nuxt.config.ts
@@ -0,0 +1,39 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  ssr: false,
+	build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-lodash',
+    'dayjs-nuxt'
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+  imports: {
+    dirs: ['stores'],
+  },
+  lodash: {
+    prefix: "_",
+    prefixSkip: false,
+    upperAfterPrefix: false,
+  },
+  
+})
