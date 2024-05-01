5aebb74e4154031f66048d4bc5fa9634fc80004d
MarszalekPiotr <marszalekpiotr183@gmail.com>
2024-05-01 10:54:30 +0200

    basic design / layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,65 @@
 <template>
-    <VApp>
-        <NuxtPage />
-    </VApp>
+    <v-app id="inspire">
+
+
+        <v-app-bar color = "layout">
+            <v-app-bar-nav-icon v-if="mobile" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-app-bar-title> JobBoard </v-app-bar-title>
+            <v-spacer> </v-spacer>
+            <VBtn icon ="mdi-theme-light-dark" title ="zmień motyw" @click="toggleTheme">  </VBtn>
+        </v-app-bar>
+
+        <v-navigation-drawer color = "layout" :order="mobile ? -1 : 0" v-model="drawer">
+            <VList>
+                <VListItem v-for ="item in menuItems" :key ="item.name" :title ="item.name" :prepend-icon="item.icon" :to ="item.url"> </VListItem>
+            </VList>
+        </v-navigation-drawer>
+
+        <v-main>
+            <div class="pa-4">
+                <NuxtPage />
+            </div>
+        </v-main>
+    </v-app>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import { useDisplay } from 'vuetify'
+import { useTheme } from 'vuetify';
+import { useStorage } from '@vueuse/core';
+
+const currentTheme  = useStorage('currentTheme','light')
+const theme = useTheme();
+const { mobile } = useDisplay();
+const drawer = ref(null)
+
+const menuItems = [
+    {
+        url:"/applications",
+        name:"moje aplikacje",
+        icon : "mdi-briefcase"
+    },
+    {
+        url:"/notifications",
+        name:"powiadomienia",
+        icon : "mdi-bell"
+    },
+
+    
+]
+
+function toggleTheme(){
+
+    let newTheme = theme.global.current.value.dark ? 'light' : 'dark';
+    theme.global.name.value = newTheme;
+    currentTheme.value = newTheme;
+
+}
+
+onMounted(() => {
+    theme.global.name.value = currentTheme.value;
+})
+
+</script>
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -3,10 +3,26 @@ import '@mdi/font/css/materialdesignicons.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import colors from 'vuetify/lib/util/colors'
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
     // ... your configuration
+    theme: {
+        defaultTheme: 'light',
+        themes: {
+          light: {
+            colors: {  
+                'layout': colors.blue.lighten1   
+            }
+          }, 
+          dark :{
+            colors: {
+                'layout': colors.blue.darken4  
+            }
+          }
+        }
+      }
   })
   app.vueApp.use(vuetify)
 })
